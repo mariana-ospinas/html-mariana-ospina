89f1f24be48e90cff26e7d70ba7ce1d30fed38a2
mariana-ospinas <123768146+mariana-ospinas@users.noreply.github.com>
2023-02-10 14:00:03 -0500

    10 de febrero

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded"), () => {
-
+const cardAdj = [
+  {
+    name: "ferxo",
+    img: "imagenes/ferxo[1].jpg"
+  },
+  {
+    name: "ferxxoo",
+    img: "imagenes/ferxxoo[1].jpg"
+  },
+  {
+    name:"mi_ra",
+    img: "imagenes/mi_ra[1].jpg"
+  },
+  {
+    name:"mini",
+    img: "imagenes/mini.jpg"
+  },
+  {
+    name:"my_2",
+    img: "imagenes/my_2[1].jpg"
+  },
+  {
+    name:"my_3",
+    img: "imagenes/my_3[1]"
+  }
+]
 });
