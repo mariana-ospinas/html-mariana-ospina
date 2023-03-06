3999ff29ede302eba20a7faba719cc98a86e82a8
mariana-ospinas <123768146+mariana-ospinas@users.noreply.github.com>
2023-03-06 12:39:11 -0500

    marzo 6

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-document.addEventListener("DOMContentLoaded"), () => {
-const cardAdj = [
-  {
-    name: "ferxo",
-    img: "imagenes/ferxo[1].jpg"
-  },
-  {
-    name: "ferxxoo",
-    img: "imagenes/ferxxoo[1].jpg"
-  },
-  {
-    name:"mi_ra",
-    img: "imagenes/mi_ra[1].jpg"
-  },
-  {
-    name:"mini",
-    img: "imagenes/mini.jpg"
-  },
-  {
-    name:"my_2",
-    img: "imagenes/my_2[1].jpg"
-  },
-  {
-    name:"my_3",
-    img: "imagenes/my_3[1]"
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "ferxo",
+      img: "imagenes/ferxo[1].jpg"
+    },
+    {
+      name: "ferxxoo",
+      img: "imagenes/ferxxoo[1].jpg"
+    },
+    {
+      name: "mi_ra",
+      img: "imagenes/mi_ra[1].jpg"
+    },
+    {
+      name: "mini",
+      img: "imagenes/mini.jpg"
+    },
+    {
+      name: "my_2",
+      img: "imagenes/my_2[1].jpg"
+    },
+    {
+      name: "my_3",
+      img: "imagenes/my_3[1]"
+    }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+
+      carta.setAttribute("src", "images/reverso.png");
+
+      carta.setAttribute("data-id", i);
+
+      carta.addEventListener("click", voltearCrata);
+
+      cuadricula.appendChild(carta);
+    }
   }
-]
-}];
-
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasEscogidas = [];
-var cartasEscogidasId = [];
-var cartasGanadas = [];
-
-function crearTablero() {
-  for (let i = 0; i < cardAdj.length; i++){
-    var carta = document.createElement("img");
-
-    carta.setAttribute("src", "images/reverso.png");
-
-    carta.setAttribute("data-id", i);
-
-    carta.addEventListener("click", voltearCrata);
-
-    cuadricula.appendChild(carta);
-  }
-}
+});
