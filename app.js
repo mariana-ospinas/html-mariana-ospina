9513da4b04df7045995858d4a4812fd132de0228
mariana-ospinas <123768146+mariana-ospinas@users.noreply.github.com>
2023-03-13 12:41:30 -0500

    13/03

--- app.js
+++ app.js
@@ -50,12 +50,19 @@ document.addEventListener("DOMContentLoaded", () => {
     var cardId = this.getAttribute("data-id");
     cartasEscogidas.push(cardAdj[cardId].name);
     cartasEscogidasId.push(cardId);
-    this.setAttribute("src",cardAdj[cardId].img);
+    this.setAttribute("src", cardAdj[cardId].img);
     if (cartasEscogidas.length === 2) {
       setTimeout(verificarPareja, 1000);
     }
   }
 
-  crearTablero();
-
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("cilck", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
+  }
 });
